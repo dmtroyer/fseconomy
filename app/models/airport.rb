c07139b67b6c42b89d73b265e4dd2e3bd1322e92
David Troyer <dmtroyer@gmail.com>
2018-05-17 12:23:32 -0400

    airport validations and specs

--- app/models/airport.rb
+++ app/models/airport.rb
@@ -1,4 +1,6 @@
 class Airport < ApplicationRecord
   alias_attribute :latitude, :lat
   alias_attribute :longitude, :lng
+
+  validates :code, uniqueness: true, length: { in: 1..4 }
 end
--- spec/models/airport_spec.rb
+++ spec/models/airport_spec.rb
@@ -1,5 +1,33 @@
 require 'rails_helper'
 
 RSpec.describe Airport, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+  describe 'code' do
+    let(:airport) { build :airport }
+
+    it 'can be 1 to 4 characters' do
+      airport.code = '1234'
+      expect(airport).to be_valid
+      airport.code = '1'
+      expect(airport).to be_valid
+    end
+
+    it 'cannot be more than 4 characters' do
+      airport.code = '12345'
+      expect(airport).to_not be_valid
+    end
+
+    it 'cannot be null' do
+      airport.code = nil
+      expect(airport).to_not be_valid
+    end
+
+    it 'must be unique' do
+      airport.code = 'KPIT'
+      airport.save
+
+      expect {
+        create :airport, code: 'KPIT'
+      }.to raise_error(ActiveRecord::RecordInvalid, "Validation failed: Code has already been taken")
+    end
+  end
 end
