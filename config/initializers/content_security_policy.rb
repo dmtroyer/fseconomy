3a991d953812e211d94c5465e2434e00a3b5f2a8
David Troyer <dmtroyer@gmail.com>
2018-05-29 16:44:55 -0400

    got hello vue working

--- config/initializers/content_security_policy.rb
+++ config/initializers/content_security_policy.rb
@@ -14,6 +14,9 @@ Rails.application.config.content_security_policy do |policy|
 
 #   # Specify URI for violation reports
 #   # policy.report_uri "/csp-violation-report-endpoint"
+
+  policy.connect_src :self, :https, "http://localhost:3035", "ws://localhost:3035" if Rails.env.development?
+
   if Rails.env.development?
     policy.script_src :self, :https, :unsafe_eval
   else
--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,8 @@
 Rails.application.routes.draw do
+  root to: 'home#index'
+
   resources :aircraft_models, param: :icao_code do
     resources :aircrafts, shallow: true
   end
   resources :airports
-  # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
