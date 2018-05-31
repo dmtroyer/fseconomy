fe9c90ede6f30cfc02932537c6dfc1f87168cf9f
David Troyer <dmtroyer@gmail.com>
2018-05-31 15:39:50 -0400

    changes to aircraft model import:
    going to go with seeds for now, as the FSE endpoint does not include icao codes.

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,22 +1,6 @@
 # This file should contain all the record creation needed to seed the database with its default values.
 # The data can then be loaded with the rails db:seed command (or created alongside the database with db:setup).
-#
-# Examples:
-#
-#   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
-#   Character.create(name: 'Luke', movie: movies.first)
 
-
-if Rails.env.development?
-  KVBW = Airport.find_or_initialize_by(name: 'Bridgewater Airpark', icao_code: 'KVBW')
-  C172 = AircraftModel.find_or_initialize_by(name: 'Cessna 172 Skyhawk', icao_code: 'C172')
-  Aircraft.create! do |aircraft|
-    aircraft.registration = 'N137DT'
-    aircraft.aircraft_model = C172
-    aircraft.home_airport = KVBW
-    aircraft.current_airport = KVBW
-    aircraft.needs_repair = false
-    aircraft.rental_cost_dry = 100
-    aircraft.rental_cost_wet = 200
-  end
-end
+aircraft_models_file = Rails.root.join('db', 'seeds', 'aircraft_models.yml')
+aircraft_models = YAML::load_file(aircraft_models_file)
+AircraftModel.import!(aircraft_models)
--- lib/tasks/fse.rake
+++
@@ -1,8 +0,0 @@
-namespace :fse do
-  desc 'Retrieve Aircraft Models from FSEconomy'
-  task :retrieve_aircraft_models do
-    uri = URI.parse("http://server.fseconomy.net/data?userkey=#{ENV['FSE_ACCESS_KEY']}&format=xml&query=aircraft&search=configs")
-    response = Net::HTTP.get_response(uri)
-    puts response.body
-  end
-end
