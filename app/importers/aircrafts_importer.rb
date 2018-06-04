7552fa61b69683f11b23258dca13e3da93e092f5
David Troyer <dmtroyer@gmail.com>
2018-06-04 17:23:18 -0400

    some changes to the importers

--- app/importers/aircrafts_importer.rb
+++ app/importers/aircrafts_importer.rb
@@ -6,52 +6,46 @@ class AircraftsImporter
 
     aircrafts = []
     invalids = []
+    xml_doc = Nokogiri::XML(File.open(Rails.root.join('storage', 'fseconomy', 'C172.xml')))
+    xml_aircrafts = xml_doc.css('Aircraft')
+    airport_codes = xml_aircrafts.map { |a| [a.css('Location').text, a.css('Home').text] }.flatten.uniq
+    airports = Airport.select(:id, :icao_code).where(icao_code: airport_codes).index_by(&:icao_code)
+
+    xml_aircrafts.each do |aircraft|
+      aircraft = Aircraft.new(
+        id: aircraft.css('SerialNumber').text,
+        registration: aircraft.css('Registration').text,
+        aircraft_model: model,
+        owner: aircraft.css('Owner').text,
+        current_airport: airports[aircraft.css('Location').text],
+        home_airport: airports[aircraft.css('Home').text],
+        sale_price: aircraft.css('SalePrice').text,
+        has_ifr_equipment: aircraft.css('Equipment').text.include?('IFR'),
+        has_autopilot: aircraft.css('Equipment').text.include?('AP'),
+        has_gps: aircraft.css('Equipment').text.include?('GPS'),
+        rental_cost_dry: aircraft.css('RentalDry').text,
+        rental_cost_wet: aircraft.css('RentalWet').text,
+        distance_bonus: aircraft.css('Bonus').text,
+        rental_time: aircraft.css('RentalTime').text,
+        rented_by: aircraft.css('RentedBy').text,
+        fuel_pct: aircraft.css('FuelPct').text,
+        needs_repair: aircraft.css('NeedsRepair').text,
+        airframe_time: fse_time_to_minutes(aircraft.css('AirframeTime').text),
+        engine_time: fse_time_to_minutes(aircraft.css('EngineTime').text),
+        time_last_100hr: fse_time_to_minutes(aircraft.css('TimeLast100hr').text)
+      )
+
+      if aircraft.validate then
+        aircrafts << aircraft
+      else
+        invalids << aircraft
+      end
+    end
 
     Aircraft.transaction do
       Aircraft.lock.where(aircraft_model: model).delete_all
-
-      xml_doc = Nokogiri::XML(File.open(Rails.root.join('storage', 'fseconomy', 'C172.xml')))
-      xml_aircrafts = xml_doc.css('Aircraft')
-
-      airport_codes = xml_aircrafts.map { |a| [a.css('Location').text, a.css('Home').text] }.flatten.uniq
-
-      airports = Airport.select(:id, :icao_code).where(icao_code: airport_codes).index_by(&:icao_code)
-
-      xml_aircrafts.each do |aircraft|
-        aircraft = Aircraft.new(
-          id: aircraft.css('SerialNumber').text,
-          registration: aircraft.css('Registration').text,
-          aircraft_model: model,
-          owner: aircraft.css('Owner').text,
-          current_airport: airports[aircraft.css('Location').text],
-          home_airport: airports[aircraft.css('Home').text],
-          sale_price: aircraft.css('SalePrice').text,
-          has_ifr_equipment: aircraft.css('Equipment').text.include?('IFR'),
-          has_autopilot: aircraft.css('Equipment').text.include?('AP'),
-          has_gps: aircraft.css('Equipment').text.include?('GPS'),
-          rental_cost_dry: aircraft.css('RentalDry').text,
-          rental_cost_wet: aircraft.css('RentalWet').text,
-          distance_bonus: aircraft.css('Bonus').text,
-          rental_time: aircraft.css('RentalTime').text,
-          rented_by: aircraft.css('RentedBy').text,
-          fuel_pct: aircraft.css('FuelPct').text,
-          needs_repair: aircraft.css('NeedsRepair').text,
-          airframe_time: fse_time_to_minutes(aircraft.css('AirframeTime').text),
-          engine_time: fse_time_to_minutes(aircraft.css('EngineTime').text),
-          time_last_100hr: fse_time_to_minutes(aircraft.css('TimeLast100hr').text)
-        )
-
-        aircraft.validate
-
-        if aircraft.valid? then
-          aircrafts << aircraft
-        else
-          invalids << aircraft
-        end
-      end
-
       Aircraft.import(aircrafts, validate: false)
-    end # transaction
+    end
 
     model.update_attribute(:last_fse_update, DateTime.now)
 
--- app/importers/airports_importer.rb
+++ app/importers/airports_importer.rb
@@ -2,12 +2,19 @@ require 'csv'
 
 class AirportsImporter
 
+  @column_mappings = {
+    'icao': 'icao_code',
+    'lon':  'lng',
+    'type': 'airport_type'
+  }
+
   def self.import
     file_path = Rails.root.join('storage', 'fseconomy', 'icaodata.csv')
     airports = CSV.read(file_path)
     columns = airports.delete_at(0)
-    columns[columns.index('icao')] = 'icao_code'
-    columns[columns.index('lon')] = 'lng'
+    @column_mappings.each do |k,v|
+      columns[columns.index(k.to_s)] = v
+    end
 
     Airport.import!(columns, airports)
 
