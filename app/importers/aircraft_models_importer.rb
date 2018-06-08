88f3a7b27c92ea31b9ec0c6b598164c323b4db93
David Troyer <dmtroyer@gmail.com>
2018-06-08 17:12:23 -0400

    models importer

---
+++ app/importers/aircraft_models_importer.rb
@@ -0,0 +1,44 @@
+class AircraftModelsImporter
+
+  def self.import(icao_code, name)
+    # file_path = Rails.root.join('storage', 'fseconomy', 'aircraft_configs.xml')
+    # xml_doc = Nokogiri::XML(File.open(file_path))
+    xml_doc = Nokogiri::XML(open(uri()))
+    xml_model = xml_doc.xpath('//xmlns:AircraftConfig[xmlns:MakeModel[text()=$name]]', nil, name: name).first
+    raise "Model name '#{name}' not found." if xml_model.nil?
+
+    AircraftModel.create!(
+      id: xml_model.css('ModelId').text,
+      name: name,
+      icao_code: icao_code,
+      additional_crew: xml_model.css('Crew').text,
+      seats: xml_model.css('Seats').text,
+      cruise_speed: xml_model.css('CruiseSpeed').text,
+      num_engines: xml_model.css('Engines').text,
+      gph: xml_model.css('GPH').text,
+      fuel_volume: calculate_fuel_volume(xml_model),
+      fuel_type: xml_model.css('FuelType').text.to_i,
+      mtow: xml_model.css('MTOW').text,
+      empty_weight: xml_model.css('EmptyWeight').text,
+      base_price: xml_model.css('Price').text,
+      engine_price: xml_model.css('EnginePrice').text
+    )
+  end
+
+  def self.calculate_fuel_volume(xml_model)
+    fuel_tanks = ['Ext1', 'LTip', 'LAux', 'LMain', 'Center1', 'Center2',
+                  'Center3', 'RMain', 'RAux', 'RTip', 'Ext2']
+    fuel_tanks.reduce(0) { |sum, tank| sum + xml_model.css(tank).text.to_i }
+  end
+
+  def self.uri
+    access_key = ENV['FSE_ACCESS_KEY']
+    raise 'FSE_ACCESS_KEY env variable not present.' unless access_key.present?
+
+    yml_path = Rails.root.join('config', 'fse.yml')
+    config = YAML.load(File.read(yml_path))[:aircraft_models]
+    params = { userkey: access_key }.merge(config[:params])
+
+    URI::HTTP.build(host: config[:host], path: config[:path], query: params.to_query)
+  end
+end
--- lib/tasks/fse.rake
+++ lib/tasks/fse.rake
@@ -2,6 +2,12 @@ namespace :fse do
 
   namespace :import do
 
+    desc 'Import Aircraft Model from FSEconomy'
+    task :aircraft_model, [:icao_code, :name] => [:environment] do |task, args|
+      AircraftModelsImporter.import(args.icao_code, args.name)
+      puts "Successfully imported #{args.icao_code} - #{args.name}"
+    end
+
     desc 'Import Aircrafts for a type from FSEconomy'
     task :aircrafts, [:icao_code] => [:environment] do |task, args|
       results = AircraftsImporter.import(args.icao_code)
